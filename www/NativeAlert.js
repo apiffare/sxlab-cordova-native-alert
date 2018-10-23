d71fda3b69ae938ad40aab0ff604853b9c3cdd2d
Evann Delaunay <evann@superextralab.com>
2018-10-23 15:13:31 +0200

    fix

--- www/NativeAlert.js
+++ www/NativeAlert.js
@@ -1,7 +1,7 @@
 /*global cordova, module*/
 
 module.exports = {
-    alert: function (msg, successCallback, errorCallback) {
+    nalert: function (msg, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "NativeAlert", "alert", [msg]);
     }
 };
